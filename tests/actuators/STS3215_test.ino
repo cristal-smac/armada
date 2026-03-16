dd5d81fbd714404101c13a452475426433e36578
Erwan <erwan.martin666@gmail.com>
2026-03-16 22:49:28 +0100

    sts3215 working

---
+++ tests/actuators/STS3215_test.ino
@@ -0,0 +1,45 @@
+#include <SCServo.h>
+
+SMS_STS sms_sts;
+// UART configuration
+#define S_RXD 16
+#define S_TXD 17
+
+int TEST_ID = 1;
+int SPEED = 1500; // Vitesse de rotation (ajuste cette valeur selon tes besoins)
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(1000000, SERIAL_8N1, S_RXD, S_TXD);
+  sms_sts.pSerial = &Serial1;
+  delay(1000);
+  Serial.println("Début du test de rotation continue");
+}
+
+void loop()
+{
+  // Faire tourner le servo à une vitesse fixe pendant 3 secondes
+  int result = sms_sts.WritePosEx(TEST_ID, 4095, SPEED, 0); // 4095 pour position, inutile???
+  if (result == -1)
+  {
+    Serial.println("Erreur : Impossible de démarrer la rotation");
+  }
+  else
+  {
+    Serial.print("Rotation à vitesse fixe pendant 3 secondes...");
+    delay(3000); // Attendre 3 secondes
+  }
+
+  // Arrêter le servo pendant 2 secondes
+  result = sms_sts.WritePosEx(TEST_ID, 0, 0, 0); // Vitesse = 0 pour arrêter
+  if (result == -1)
+  {
+    Serial.println("Erreur : Impossible d'arrêter le servo");
+  }
+  else
+  {
+    Serial.println("Servo arrêté pendant 2 secondes...");
+    delay(2000); // Attendre 2 secondes
+  }
+}
